d757ca032ca6884b6f4f6c7d79427e1302f52e2a
Pega <jehyun.lee@gmail.com>
2019-10-29 12:31:20 +0900

    Delete run03.sh

--- 03/run03.sh
+++
@@ -1,4 +0,0 @@
-export DISPLAY=192.168.99.1:0.0
-#set-variable -name DISPLAY -value 192.168.99.1:0.0
-docker run -ti --rm --name=firefox -e DISPLAY=$DISPLAY --privileged -v //c/Arbeitplatz/16_dockerplace/:/home/jehyunlee/workplace test:03firefox /bin/bash
-docker run -ti --rm --name=firefox -e DISPLAY=$DISPLAY --privileged -v //c/Arbeitplatz/16_dockerplace/:/home/jehyunlee/workplace test:03firefox jupyter notebook --ip=0.0.0.0 --port=8888 --allow-root
